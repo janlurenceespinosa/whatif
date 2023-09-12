76c17bf7147679f939491b33a27b7afd3177ab53
janlurenceespinosa <144762683+janlurenceespinosa@users.noreply.github.com>
2023-09-12 12:52:47 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const main = document.querySelector('.main');
+const cancel = document.querySelector('.cancel');
+const msg = document.querySelector('#msg');
+
+while (true) {
+
+        alert("What if gusto kita PAT");
+        alert('What if Tayo nalang PAT');
+        alert('I like You na yawa HAHAHA ');
+        main.style.display = 'block';
+        break;
+    
+}
